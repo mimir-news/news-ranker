0fb33c29ee3282706730c430e9f33f9af42fead5
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-10-28 22:54:25 +0100

    Added test coveratg for article_service

--- cmd/article_service_test.go
+++ cmd/article_service_test.go
@@ -1,6 +1,145 @@
 package main
 
-import "github.com/mimir-news/pkg/schema/news"
+import (
+	"testing"
+	"time"
+
+	mqtest "github.com/mimir-news/pkg/mq/mqtest"
+	"github.com/mimir-news/pkg/schema/news"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleScrapedArticleMessage_Success(t *testing.T) {
+	assert := assert.New(t)
+
+	scrapedArticle := getTestScrapedArticle()
+
+	message := mqtest.NewMessage(scrapedArticle, false, false)
+
+	oldReferers := []news.Referer{
+		news.Referer{
+			ID:            "r-0",
+			ExternalID:    "e-id-0",
+			FollowerCount: 1000,
+			ArticleID:     "a-0",
+		},
+		news.Referer{
+			ID:            "r-1",
+			ExternalID:    "e-id-1",
+			FollowerCount: 1000,
+			ArticleID:     "a-0",
+		},
+	}
+
+	articleRepo := &mockArticleRepo{
+		articleReferers:        oldReferers,
+		findArticleReferersErr: nil,
+		saveScrapedArticleErr:  nil,
+		articleSubjects:        nil, // Set up to prevent clusterering which is not in scope for the test.
+		findArticleSubjectsErr: mockError,
+	}
+	mockEnv := &env{
+		config:      Config{TwitterUsers: 6000},
+		articleRepo: articleRepo,
+	}
+
+	err := mockEnv.handleScrapedArticleMessage(message)
+	assert.Nil(err)
+
+	assert.Equal(scrapedArticle.Article.ID, articleRepo.findArticleReferersArg)
+	assertScore(0.5, articleRepo.saveScrapedArticleArg.Article.ReferenceScore, t)
+}
+
+func TestHandleScrapedArticleMessage_FailedParse(t *testing.T) {
+	message := mqtest.NewMessage([]byte("will not parse"), false, false)
+	mockEnv := &env{}
+	err := mockEnv.handleScrapedArticleMessage(message)
+	assert.NotNil(t, err)
+}
+
+func TestHandleScrapedArticleMessage_FailedDBInteractions(t *testing.T) {
+	assert := assert.New(t)
+
+	scrapedArticle := getTestScrapedArticle()
+
+	message := mqtest.NewMessage(scrapedArticle, false, false)
+
+	articleRepoNoReferers := &mockArticleRepo{
+		articleReferers:        nil,
+		findArticleReferersErr: mockError,
+	}
+
+	mockEnv := &env{
+		config:      Config{TwitterUsers: 2000},
+		articleRepo: articleRepoNoReferers,
+	}
+
+	err := mockEnv.handleScrapedArticleMessage(message)
+	assert.Nil(err)
+	assert.Equal(scrapedArticle.Article.ID, articleRepoNoReferers.findArticleReferersArg)
+	assert.Equal("", articleRepoNoReferers.saveScrapedArticleArg.Article.ID)
+
+	oldReferers := []news.Referer{
+		news.Referer{
+			ID:            "r-new",
+			ExternalID:    "e-id-new",
+			FollowerCount: 1000,
+			ArticleID:     "a-0",
+		},
+	}
+
+	articleRepoFailedSave := &mockArticleRepo{
+		articleReferers:        oldReferers,
+		findArticleReferersErr: nil,
+		saveScrapedArticleErr:  mockError,
+	}
+
+	mockEnv.articleRepo = articleRepoFailedSave
+
+	err = mockEnv.handleScrapedArticleMessage(message)
+	assert.Nil(err)
+	assert.Equal(scrapedArticle.Article.ID, articleRepoFailedSave.findArticleReferersArg)
+	assert.Equal(scrapedArticle.Article.ID, articleRepoFailedSave.saveScrapedArticleArg.Article.ID)
+	assertScore(0.5, articleRepoFailedSave.saveScrapedArticleArg.Article.ReferenceScore, t)
+}
+
+func getTestScrapedArticle() news.ScrapedArticle {
+	return news.ScrapedArticle{
+		Article: news.Article{
+			ID:    "a-0",
+			URL:   "http://url.com",
+			Title: "title-0",
+			Body:  "Body text",
+			Keywords: []string{
+				"w0",
+				"w1",
+			},
+			ArticleDate: time.Now(),
+		},
+		Subjects: []news.Subject{
+			news.Subject{
+				ID:        "s-0",
+				Symbol:    "S0",
+				Name:      "subject-0",
+				Score:     0.1,
+				ArticleID: "a-0",
+			},
+			news.Subject{
+				ID:        "s-1",
+				Symbol:    "S1",
+				Name:      "subject-1",
+				Score:     0.2,
+				ArticleID: "a-0",
+			},
+		},
+		Referer: news.Referer{
+			ID:            "r-new",
+			ExternalID:    "e-id-new",
+			FollowerCount: 1000,
+			ArticleID:     "a-0",
+		},
+	}
+}
 
 type mockArticleRepo struct {
 	findByURLArg     string
